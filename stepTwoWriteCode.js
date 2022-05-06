160c36124ebda7625ca6101be4fe4a2439d92090
Tori Daughton <toriddaughton@gmail.com>
2022-05-06 15:54:53 -0600

    forgot the space

--- stepTwoWriteCode.js
+++ stepTwoWriteCode.js
@@ -1,18 +1,18 @@
 // const sumZero = (arr) => {
-//     let solution = false
 //     for(let i = 0; i < arr.length; i++){
-//         for(let k = i+1; k < arr.length; k++){
+//         for(let j = i+1; j < arr.length; j++){
 //             // console.log(i,k)
-//         if(arr[i] + arr[k] === 0){
-//             solution = true;
+//         if(arr[i] + arr[j] === 0){
+//             result = true;
 //             }
 //         }
 //     }  
-//     return solution
+//     return false
 // }
 // console.log(sumZero([1,2,3,-2]))
-// I believe the runtime of this code would be O(n^2)
+// I believe the runtime of this code would be O(n^2) time complexity
 // this is because the loops are both considered O(n), and with one nested within the other (and dependent of one another) this causes it to be quadratic
+// space complexity is O(1) because it is pointing to location in memory
 
 // const hasUniqueChars = (str) => {
 //     for(let i = 0; i <= str.length; i++){
@@ -26,6 +26,7 @@
 // }
 // console.log(hasUniqueChars("Moonday"))
 // The runtime of this code is also: O(n^2)
+//  space complexity: O(n)
 
 // const pangram = (str) => {
 //     let regex = /([a-z])(?!.*\1)/gi;
@@ -33,7 +34,7 @@
 //     }
 // console.log(pangram("The quick brown fox jumps over the lazy dog!"))
 // In this format, I believe this would be O(n) because while it is comparing against a constant number (26) my understanding is that match is looping through the string to check if it matches regex.
-
+// space complexity: O(n) because depends on length of string
 
 // const findLongestWord = (arr) => {
 //     longestWord = 0
@@ -46,3 +47,4 @@
 // }
 // console.log(findLongestWord(["hi", "hello", "elephant"]))
 // I believe this one would be O(n) because it is depending on the length of the array in the for loop
+// space complexity O(1)
